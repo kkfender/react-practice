1397eba7929b5624ee81ca07cce47860b0da8533
kkfender <ibanez19851030@yahoo.co.jp>
2025-01-18 18:13:13 +0900

    useState

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
 import { ColoredMessage } from "./components/ColoredMessage"
+
 export const App = () => {
+    // stateの定義
+    const [num, setNum] = useState(0);
+
+    const onClickButton = () => {
+        alert();
+        setNum(num + 1);
+    };
+
     return(
     <>
         <h1>こんにちは！</h1>;
         <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
         <ColoredMessage color="pink">元気です</ColoredMessage>
+        <button onClick={onClickButton}>ボタン</button>
+        <p>{num}</p>
     </>
     )
 }
