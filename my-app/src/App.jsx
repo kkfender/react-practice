8fed5f978a607b72b4a376cd1a1b02b814da9333
kkfender <ibanez19851030@yahoo.co.jp>
2025-01-29 02:18:46 +0900

    useCallback

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from "react"
+import { useState, memo, useCallback } from "react"
 import { Child1 } from "./components/Child"
 import { Child4 } from "./components/Child4"
 
@@ -12,11 +12,14 @@ export const App = memo(() => {
         setNum(num + 1);
     };
 
+    const onClickReset = useCallback(() => {
+        setNum(0);
+    }, []);
     return(
     <>
         <button onClick={onClickButton}>ボタン</button>
         <p>{num}</p>
-        <Child1></Child1>
+        <Child1 onClickReset={onClickReset}></Child1>
         <Child4></Child4>
     </>
     )
